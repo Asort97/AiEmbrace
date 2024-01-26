fd607dd1146904b66de894c784dd47f2023e9a6f
Malik <Lorsanov>
2024-01-26 20:12:40 +0300

    auth, fix design, log and reg

--- Assets/Scripts/Auntification.cs
+++ Assets/Scripts/Auntification.cs
@@ -2,16 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using System.Linq;
+using System;
 
 public class Auntification : MonoBehaviour
 {
-    public void Login()
+    [SerializeField] private GameObject welcomeMenu;
+    [SerializeField] private GameObject loginMenu;
+    [SerializeField] private GameObject registrationMenu;
+
+    [SerializeField] private TMP_InputField login_emailField;
+    [SerializeField] private TMP_InputField login_passwordField;
+
+    [SerializeField] private TMP_InputField register_emailField;
+    [SerializeField] private TMP_InputField register_passwordField;
+
+    public void ToLoginMenu()
     {
-        SceneManager.LoadScene("GameScene");     
+        welcomeMenu.SetActive(false);
+        registrationMenu.SetActive(false);
+        loginMenu.SetActive(true);
     }
 
-    public void Register()
+    public void ToRegisterMenu()
     {
-        // Register new member 
+        welcomeMenu.SetActive(false);
+        loginMenu.SetActive(false);
+        registrationMenu.SetActive(true);
     }
+
+    public async void Register()
+    {
+        RegisterResponse response = await ClientAPI.instance.Register(register_emailField.text, register_passwordField.text);
+
+        Debug.Log(register_emailField.text);
+        Debug.Log(register_passwordField.text);
+
+        if(response.success)
+        {
+            Debug.Log($"Success register");
+
+            LoginResponse responseLogin = await ClientAPI.instance.Login(register_emailField.text, register_passwordField.text);
+
+            if(responseLogin.success)
+            {
+                SceneManager.LoadScene("GameScene");
+            }
+        }
+        else
+        {
+            string errors = string.Join("|", response.errors.SelectMany(kv => kv.Value));
+
+            Debug.Log($"Non Success register {errors}");
+        }
+    }
+
+    public async void Login()
+    {
+        LoginResponse response = await ClientAPI.instance.Login(login_emailField.text, login_passwordField.text);
+
+        if(response.success)
+        {
+            Debug.Log($"Success login");
+            SceneManager.LoadScene("GameScene");
+        }
+        else
+        {
+            Debug.Log($"Non Success login");
+        }
+    }
+
 }
