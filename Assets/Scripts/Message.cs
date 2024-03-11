d22598b1b447d507f2772d0c6f821ca60e067527
Malik <Lorsanov>
2024-03-11 12:32:10 +0300

    Backup

--- Assets/Scripts/Message.cs
+++ Assets/Scripts/Message.cs
@@ -75,7 +75,7 @@ public class Message : MonoBehaviour
 
     public void EnableReactionPanel(Message message)
     {
-        if(isPlayer)
+        if(!isPlayer)
         {
             OnClick?.Invoke(this);
 
